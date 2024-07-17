2cc4316f15c563aec063a700581ba401a1d91010
Arocano <arocano6813@gmail.com>
2024-07-17 15:31:56 -0500

    login

---
+++ app/login/page.jsx
@@ -0,0 +1,14 @@
+
+import LoginForm from "@public/components/login/LoginForm";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <main >
+      
+       
+          <LoginForm />
+      
+    </main>
+  );
+}
